9fa85f3e42c4a3d3c43d9aef99d86a9b6da90f63
alejozuleta9217 <jazuletam@dane.gov.co>
2023-01-14 21:46:27 -0500

    five commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Data from "./page/data";
-import { ThemeProvider } from '@material-ui/core/styles';
 
 export default function App() {
   return(
-    <ThemeProvider>
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Data/>}/>
       </Routes>
     </BrowserRouter>
-    </ThemeProvider>
   )
 }
